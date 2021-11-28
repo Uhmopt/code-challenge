8455aef52bd28ff55e969cac47b5e30da2607c38
Uhmopt <qethum@gmail.com>
2021-11-28 17:45:59 +1100

    [New] environment for backend production

--- src/lib/global.js
+++ src/lib/global.js
@@ -1,8 +1,13 @@
+// build control
+export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+
 export const APP_NAME = "Code Challenge";
 
 export const COUNT_PER_PAGE = 12;
 
-export const SERVER_URI = "http://localhost:4000/";
+export const SERVER_URI = IS_DEVELOPMENT
+  ? "http://localhost:4000/"
+  : "https://ivan-code-challenge.herokuapp.com/";
 
 export const RANDOM_IMAGE_LG =
   "https://source.unsplash.com/1600x900/?film,movie";
