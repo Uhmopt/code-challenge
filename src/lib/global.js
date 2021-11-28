374cf74b93464e9c3f23d6950a4591841b64bbff
Uhmopt <qethum@gmail.com>
2021-11-28 21:37:15 +1100

    [Update] api url

--- src/lib/global.js
+++ src/lib/global.js
@@ -7,7 +7,7 @@ export const COUNT_PER_PAGE = 12;
 
 export const SERVER_URI = IS_DEVELOPMENT
   ? "http://localhost:4000/"
-  : "https://ivan-code-challenge.herokuapp.com/";
+  : "https://ivan-code-challenge-api.herokuapp.com/";
 
 export const RANDOM_IMAGE_LG =
   "https://source.unsplash.com/1600x900/?film,movie";
