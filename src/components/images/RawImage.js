6858f4f51bdffa42cae90baaeeb44bf3fafcaa9c
Uhmopt <qethum@gmail.com>
2021-11-30 02:47:44 +1100

    [Update] key

--- src/components/images/RawImage.js
+++ src/components/images/RawImage.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function RawImage({ src = "", alt = "", ...props }) {
-  return <img src={src ?? ""} alt={alt ?? ""} {...props} />;
+  return (
+    <img
+      key={"raw_image" + src + alt}
+      src={src ?? ""}
+      alt={alt ?? ""}
+      {...props}
+    />
+  );
 }
--- src/pages/movies/forms/MoviesGrid.js
+++ src/pages/movies/forms/MoviesGrid.js
@@ -8,9 +8,8 @@ export default function MoviesGrid({ data = [] }) {
     <div>
       <Grid container spacing={4}>
         {formatArray(data).map((movie, movieIndex) => {
-          const rand = String(Math.random());
           return (
-            <Grid key={rand + movieIndex} item lg={3} md={4} sm={6} xs={12}>
+            <Grid key={movieIndex} item lg={3} md={4} sm={6} xs={12}>
               <MovieCard data={movie} />
             </Grid>
           );
